bd00b890dd84a2565ed58cf7cb04cf95e1620da4
Jay <juliuschilango@gmail.com>
2024-05-14 16:09:40 +0300

    lesson 4

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,11 +1,19 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+        val nextBtn = findViewById<TextView>(R.id.nextBtn)
+
+        nextBtn.setOnClickListener {
+            val LeagueIntent = Intent(this,LeagueActivity::class.java)
+            startActivity(LeagueIntent)
+        }
     }
 }
--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.swoosh
 
-import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
-import com.example.swoosh.databinding.ActivityMainBinding
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val getStartedBtn = findViewById<TextView>(R.id.nextBtn)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this,LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
